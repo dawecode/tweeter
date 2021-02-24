832627cd77c08cfd9f3c75633ebc00b27ad968fe
Catherine Dawe <dawe.cm@gmail.com>
2021-02-24 14:38:52 -0500

    add client js to render tweets and create tweets from fake data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+ 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+ 
+const createTweetElement = function(tweetObj) {
+  const timeStamp = moment(tweetObj.created_at).fromNow();
+  let $tweet = $(` 
+  <article>
+  <header>
+    <span class="user"><img class="user" src="${tweetObj.user.avatars}" alt="Face Logo Image">
+    <p>${tweetObj.user.name}</p></span>
+    <p class="handle">${tweetObj.user.handle}</p>
+  </header>
+    <p class="profile-tweet">${tweetObj.content.text}</p>
+  
+  <footer>
+    <span class ="time">${timeStamp}</span>
+    <span class ="buttons"> 
+      <i class="fas fa-flag"></i> 
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      </article>` );
+
+  return $tweet;
+}
+
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for(let tweet of tweets){
+    const newTweet = createTweetElement(tweet)
+    $('#tweets-container').append(newTweet);
+  }
+  
+}
+
+renderTweets(data);
+
+});
